c81fbb2e6f45a300742574b1efce0b34b1a6fa52
sunjinbo <sunjinbo@360.cn>
2020-08-14 20:56:26 +0800

    merge codes

---
+++ 1.两数之和.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            map.put(nums[i], i);
+        }
+
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; ++i) {
+            int minus = target - nums[i];
+            if (map.containsKey(minus) && i != map.get(minus)) {
+                result[0] = i;
+                result[1] = map.get(minus);
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
