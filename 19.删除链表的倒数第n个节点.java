83ad4a8b920de1ea52b49a47c2340f44cc85ae10
sunjinbo <sunjinbo@360.cn>
2020-08-24 18:16:08 +0800

    删除链表的倒数第N个节点

---
+++ 19.删除链表的倒数第n个节点.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=java
+ *
+ * [19] 删除链表的倒数第N个节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head.next == null && n == 1) return null;
+
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode start = pre, end = pre;
+
+        while (n != 0) {
+            start = start.next;
+            n--;
+        }
+        while(start.next != null) {
+            start = start.next;
+            end = end.next;
+        }
+        end.next = end.next.next;
+        return pre.next;
+    }
+}
+// @lc code=end
+
