3a5b58122d5d7e36d096cdf3f280f691fdc72994
sunjinbo <sunjinbo@360.cn>
2020-08-27 20:45:27 +0800

    旋转图像

---
+++ 48.旋转图像.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=48 lang=java
+ *
+ * [48] 旋转图像
+ */
+
+// @lc code=start
+class Solution {
+    public void rotate(int[][] matrix) {
+        if(matrix.length == 0 || matrix.length != matrix[0].length) {
+            return;
+        }
+        int nums = matrix.length;
+        //先沿右上 - 左下的对角线翻转（270° +270°+ 一次镜像）
+        for (int i = 0; i < nums; ++i){
+            for (int j = 0; j < nums - i; ++j){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[nums - 1 - j][nums - 1 - i];
+                matrix[nums - 1 - j][nums - 1 - i] = temp;
+            }
+        }
+
+        // 再沿水平中线上下翻转（-180° +−180°+ 一次镜像）
+        for (int i = 0; i < (nums >> 1); ++i){
+            for (int j = 0; j < nums; ++j){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[nums - 1 - i][j];
+                matrix[nums - 1 - i][j] = temp;
+            }
+        }
+    }
+}
+// @lc code=end
+
