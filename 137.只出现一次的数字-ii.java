477f555889fdd8ffa7149c97009e26a689c2e8dc
sunjinbo <sunjinbo@360.cn>
2020-09-22 11:28:44 +0800

    只出现一次的数字 II

--- 137.只出现一次的数字-ii.java
+++ 137.只出现一次的数字-ii.java
@@ -7,11 +7,25 @@
 // @lc code=start
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
-        for (int i = 0; i < nums.length; ++i) {
-            ans ^= nums[i];
+        int seenOnce = 0, seenTwice = 0;
+
+        for (int num : nums) {
+          // first appearence: 
+          // add num to seen_once 
+          // don't add to seen_twice because of presence in seen_once
+    
+          // second appearance: 
+          // remove num from seen_once 
+          // add num to seen_twice
+    
+          // third appearance: 
+          // don't add to seen_once because of presence in seen_twice
+          // remove num from seen_twice
+          seenOnce = ~seenTwice & (seenOnce ^ num);
+          seenTwice = ~seenOnce & (seenTwice ^ num);
         }
-        return ans;
+    
+        return seenOnce;
     }
 }
 // @lc code=end
