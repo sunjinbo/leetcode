25eba0f5410c85f71599d73b3c24c0daf92456c8
sunjinbo <sunjinbo@360.cn>
2020-10-28 22:11:16 +0800

    合并两个有序链表

---
+++ 21.合并两个有序链表.java
@@ -0,0 +1,51 @@
+import javax.management.ListenerNotFoundException;
+
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode l = new ListNode();
+        ListNode p = l;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+
+        if (l1 != null) {
+            p.next = l1;
+        }
+
+        if (l2 != null) {
+            p.next = l2;
+        }
+
+        return l.next;
+    }
+}
+// @lc code=end
+
