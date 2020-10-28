1935a65e3cf634dfe6fb79ee16caa6074966adf4
sunjinbo <sunjinbo@360.cn>
2020-10-28 23:20:35 +0800

    最大子序和

---
+++ 53.最大子序和.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=java
+ *
+ * [53] 最大子序和
+ */
+
+// @lc code=start
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+}
+// @lc code=end
+
