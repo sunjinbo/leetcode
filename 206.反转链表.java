291a3e6cdb6a1023795491eafb0a8fbde0efa5ae
sunjinbo <sunjinbo@360.cn>
2021-01-21 18:38:34 +0800

    反转链表

---
+++ 206.反转链表.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=java
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
+}
+// @lc code=end
+
