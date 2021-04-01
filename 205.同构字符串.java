266a25217dc017f82db0efd6497ac71674d20df6
sunjinbo <sunjinbo@360.cn>
2021-04-01 20:30:31 +0800

    同构字符串

---
+++ 205.同构字符串.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=205 lang=java
+ *
+ * [205] 同构字符串
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        char[] chars = s.toCharArray();
+        char[] chart = t.toCharArray();
+        int[] preIndexOfs = new int[256];
+        int[] preIndexOft = new int[256];
+        for (int i = 0; i < chars.length; i++) {
+            if (preIndexOfs[chars[i]] != preIndexOft[chart[i]]) {
+                return false;
+            }
+            preIndexOfs[chars[i]] = i + 1;
+            preIndexOft[chart[i]] = i + 1;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
