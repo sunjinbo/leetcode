51f385b323b70948d59ededae578bdccfb0e9767
sunjinbo <sunjinbo@360.cn>
2021-05-14 13:24:06 +0800

    移动零

---
+++ 283.移动零.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=java
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+class Solution {
+    public void moveZeroes(int[] nums) {
+        for (int i = 0; i < nums.length; ++i) {
+            for (int j = i + 1; j < nums.length; ++j) {
+                if (nums[i] == 0 && nums[j] != 0) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+    }
+}
+// @lc code=end
+
