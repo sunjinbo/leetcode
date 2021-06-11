c85bebf1945cdb46c1305f7394af2506488cc6b0
sunjinbo <sunjinbo@360.cn>
2021-06-11 18:12:01 +0800

    Excel表列名称

--- 168.excel表列名称.java
+++ 168.excel表列名称.java
@@ -7,7 +7,17 @@
 // @lc code=start
 class Solution {
     public String convertToTitle(int columnNumber) {
-
+        int n = columnNumber;
+        char[] chart = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z'};
+        String s = "";
+        while (n > 0) {
+            int a = (n-1)%26;
+            char tem = chart[a];
+            s = tem + s;
+            n = n - 1;
+            n = n / 26;
+        }
+        return s;
     }
 }
 // @lc code=end
