7b9b80cacbec1deb1d86bd166d6c21f698ad7181
sunjinbo <sunjinbo@360.cn>
2021-08-11 12:13:04 +0800

    复写零

---
+++ 1089.复写零.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=1089 lang=java
+ *
+ * [1089] 复写零
+ */
+
+// @lc code=start
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        for (int i = 0; i < arr.length; ++i) {
+            if (arr[i] == 0) {
+                for (int j = arr.length - 1; j > i; --j) {
+                    arr[j] = arr[j - 1];
+                }
+                if (i + 1 < arr.length) {
+                    arr[i + 1] = 0;
+                }
+                i += 1;
+            }
+        }
+    }
+}
+// @lc code=end
+
