e237579edda340020c54c2ee2f7763c696d99944
sunjinbo <sunjinbo@360.cn>
2021-09-23 14:42:10 +0800

    提莫攻击

---
+++ 495.提莫攻击.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=495 lang=java
+ *
+ * [495] 提莫攻击
+ */
+
+// @lc code=start
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int n = timeSeries.length;
+        if (n == 0) return 0;
+
+        int total = 0;
+        for(int i = 0; i < n - 1; ++i)
+          total += Math.min(timeSeries[i + 1] - timeSeries[i], duration);
+        return total + duration;
+    }
+}
+// @lc code=end
+
