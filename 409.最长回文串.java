24fce33d3c080680a9b7608866afbaec1c04b5d6
sunjinbo <sunjinbo@360.cn>
2021-09-30 18:48:16 +0800

    最长回文串

---
+++ 409.最长回文串.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=409 lang=java
+ *
+ * [409] 最长回文串
+ */
+
+// @lc code=start
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];
+        int length = s.length();
+        for (int i = 0; i < length; ++i) {
+            char c = s.charAt(i);
+            count[c]++;
+        }
+
+        int ans = 0;
+        for (int v: count) {
+            ans += v / 2 * 2;
+            if (v % 2 == 1 && ans % 2 == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
