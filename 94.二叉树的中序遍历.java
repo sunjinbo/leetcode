c4b1b5b0016e0b0d5cc291130aa5c8c7edf2f679
sunjinbo <sunjinbo@360.cn>
2021-10-08 16:06:18 +0800

    二叉树的中序遍历

---
+++ 94.二叉树的中序遍历.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=94 lang=java
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        inorderTraversal(root, ans);
+        return ans;
+    }
+
+    public void inorderTraversal(TreeNode root, List<Integer> ans) {
+        if (root != null) {
+            inorderTraversal(root.left, ans);
+            ans.add(root.val);
+            inorderTraversal(root.right, ans);
+        }
+    }
+}
+// @lc code=end
+
