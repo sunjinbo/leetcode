fb28912a924baf8082627721ade6693fe26c431f
sunjinbo <sunjinbo@outlook.com>
2021-11-10 11:17:55 +0800

    二叉树的所有路径

---
+++ 257.二叉树的所有路径.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=257 lang=java
+ *
+ * [257] 二叉树的所有路径
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<String>();
+        constructPaths(root, "", paths);
+        return paths;
+    }
+
+    public void constructPaths(TreeNode root, String path, List<String> paths) {
+        if (root != null) {
+            StringBuffer pathSB = new StringBuffer(path);
+            pathSB.append(Integer.toString(root.val));
+            if (root.left == null && root.right == null) {  // 当前节点是叶子节点
+                paths.add(pathSB.toString());  // 把路径加入到答案中
+            } else {
+                pathSB.append("->");  // 当前节点不是叶子节点，继续递归遍历
+                constructPaths(root.left, pathSB.toString(), paths);
+                constructPaths(root.right, pathSB.toString(), paths);
+            }
+        }
+    }
+}
+// @lc code=end
+
