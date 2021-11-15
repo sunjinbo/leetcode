d77f0f1ee4c7d6d87f68b8012b84ad80dfb806e1
sunjinbo <sunjinbo@outlook.com>
2021-11-15 12:44:05 +0800

    二叉搜索树的范围和

---
+++ 938.二叉搜索树的范围和.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=938 lang=java
+ *
+ * [938] 二叉搜索树的范围和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        int sum = 0;
+        if (root != null) {
+            if (root.val >= low && root.val <= high) {
+                sum += root.val;
+            }
+            sum += rangeSumBST(root.left, low, high);
+            sum += rangeSumBST(root.right, low, high);
+        }
+        return sum;
+    }
+}
+// @lc code=end
+
