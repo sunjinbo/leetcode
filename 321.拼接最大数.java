384d6befddb143fe9fdc7c8a42c465281f1535da
sunjinbo <sunjinbo@outlook.com>
2021-12-23 14:16:59 +0800

    拼接最大

---
+++ 321.拼接最大数.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=321 lang=java
+ *
+ * [321] 拼接最大数
+ */
+
+// @lc code=start
+class Solution {
+    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+
+    }
+}
+// @lc code=end
+
