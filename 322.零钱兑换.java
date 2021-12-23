f693546d1c63872ee13eef5f89ae209529151c36
sunjinbo <sunjinbo@outlook.com>
2021-12-23 17:29:21 +0800

    零钱兑

--- 322.零钱兑换.java
+++ 322.零钱兑换.java
@@ -7,18 +7,24 @@
 // @lc code=start
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int max = amount + 1;
+        // dp[i] 为组合成 i 时需要的最少硬币数
         int[] dp = new int[amount + 1];
-        Arrays.fill(dp, max);
         dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (coins[j] <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+        for (int i = 1; i < amount + 1; ++i) {
+            dp[i] = amount + 1;
+        }
+
+        for (int i = 1; i <= amount; ++i) {
+            for (int j = 0; j < coins.length; ++j) {
+                // 当前硬币的大小
+                int coin = coins[j];
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+
+        return dp[amount] >= amount + 1 ? -1 : dp[amount];
     }
 }
 // @lc code=end
